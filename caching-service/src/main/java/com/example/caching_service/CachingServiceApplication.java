87eae10ce943920eaf5c50baf2880af26c8366d2
Vijayadhithram <vijayadhithram@gmail.com>
2025-11-21 16:27:12 +0530

    Commit Caching-Service

---
+++ caching-service/src/main/java/com/example/caching_service/CachingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.caching_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingServiceApplication.class, args);
+	}
+
+}
